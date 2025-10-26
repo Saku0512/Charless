d634a3cb753b1962b7d1339bb0dd69c22a57a2d5
Saku0512 <moonandstar118@gmail.com>
2025-10-26 11:49:34 +0900

    faat: 文字列出力実装

--- src/compiler.c
+++ src/compiler.c
@@ -141,6 +141,7 @@ int main(int argc, char *argv[]) {
         fprintf(out_file, ".data\n");
         fprintf(out_file, "memory: .zero %d\n", MEMORY_SIZE * 8); // long = 8 bytes
         fprintf(out_file, "output_format: .asciz \"%%ld\\n\"\n");
+        fprintf(out_file, "char_format: .asciz \"%%c\"\n");
         fprintf(out_file, "\n");
         fprintf(out_file, ".text\n");
         fprintf(out_file, "main:\n");
@@ -161,7 +162,44 @@ int main(int argc, char *argv[]) {
                     fprintf(out_file, "    pop rbp\n");
                     fprintf(out_file, "    ret\n");
                     break;
+                
+                case 01: // PRINT_STRING
+                    if (!consume_separator()) {
+                        fprintf(stderr, "Error: Expected '20' after 01\n");
+                        exit(1);
+                    }
+                    fprintf(out_file, "    # PRINT_STRING\n");
 
+                    while (strncmp(ip, "20", 2) != 0 && *ip != '\0') {
+                        char len_str[2] = {*ip, '\0'};
+                        ip++;
+                        int len = atoi(len_str);
+
+                        if (len <= 0 || len > 10) { // Basic validation
+                            fprintf(stderr, "Error: Invalid length for character code\n");
+                            exit(1);
+                        }
+
+                        char* code_str = malloc(len + 1);
+                        strncpy(code_str, ip, len);
+                        code_str[len] = '\0';
+                        ip += len;
+
+                        long val = atol(code_str);
+                        free(code_str);
+
+                        fprintf(out_file, "    mov rsi, %ld\n", val);
+                        fprintf(out_file, "    lea rdi, char_format[rip]\n");
+                        fprintf(out_file, "    mov al, 0\n");
+                        fprintf(out_file, "    call printf\n");
+                    }
+
+                    if (!consume_separator()) {
+                        fprintf(stderr, "Error: Expected '20' after string data\n");
+                        exit(1);
+                    }
+                    break;
+                
                 case 02: // PRINT
                     if (!consume_separator()) {
                         fprintf(stderr, "Error: Expected '20' after 02\n");
